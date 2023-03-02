ef5892eba5b8055d1dcc4400b2080140879407d6
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-03-02 17:43:16 +0000

    Add arrows

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,10 +4,29 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers (a, b) {
+    // Code Block
+    return a + b;
+}
+
+let sum = addTwoNumbers(12, 7);
+console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers1 = (a, b) => {
+    // Code Block
+    return a + b;
+}
+
+let sum1 = addTwoNumbers1(2, 7);
+console.log(sum1);
+
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(12, 2);
+console.log(sum2);
+
 
 // Implicit Returns
 
